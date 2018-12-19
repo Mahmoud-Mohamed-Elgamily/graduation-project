b16e3238e380e804abd35da61c5ad337b3c2c667
Omran6688 <mohamedomran6688@gmail.com>
2018-12-19 22:13:35 +0200

    1

--- UCMS/settings.py
+++ UCMS/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'UCMS',
         'USER': 'postgres',
-        'PASSWORD': 'z2131996eus',
+        'PASSWORD': '0573631522',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
