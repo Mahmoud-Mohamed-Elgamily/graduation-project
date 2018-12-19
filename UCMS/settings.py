493a32d443f9280b384f35cd5ddc61b698f2fd1b
mahmoed mohamed <mahmoedmohamed19@gmail.com>
2018-12-19 03:13:01 +0200

    adding postgres

--- UCMS/settings.py
+++ UCMS/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'UCMS.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'UCMS',
+        'USER': 'postgres',
+        'PASSWORD': 'z2131996eus',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
