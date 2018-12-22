088144e7e79a6f343b8910052fb6737bc14116e8
mahmoed mohamed <mahmoedmohamed19@gmail.com>
2018-12-22 15:00:39 +0200

    update

--- database/views.py
+++ database/views.py
@@ -4,4 +4,12 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def stud(request):
-    return HttpResponse("ok")
+    user = request.user
+    if user.is_authenticated and user.is_teacher:
+        return redirect('doctor:home')
+    elif user.is_authenticated and user.is_student:
+        return redirect('student:home')
+    elif user.is_authenticated and user.is_assistant:
+        return redirect('assistant:home')
+    else:
+        return render(request,'login.html')
